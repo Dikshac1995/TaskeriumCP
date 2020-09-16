83f55c08a05b6b919de3cb321f59608032999fc0
Amit chouhan <amit.chouhan@openbrix.co.uk>
2020-09-16 21:06:37 +0530

    changes for lang

--- App/Components/Screens/ContentType/LanguageModal/LanguageModal.js
+++ App/Components/Screens/ContentType/LanguageModal/LanguageModal.js
@@ -11,7 +11,7 @@ import {
     ActivityIndicator,
     FlatList,
     Linking,
-    StyleSheet,Modal,
+    StyleSheet, Modal,
 } from 'react-native';
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { Container, Header, Content, ListItem, Radio, Right, Left } from 'native-base';
@@ -19,81 +19,85 @@ import { setTranslation } from "../../../../Redux/Actions/LocalizeAction"
 import {
     colors,
     fonts,
-    sty,images
+    sty, images
 } from "../../../../Theme";
 import styles from './Styles'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import FastImage from 'react-native-fast-image'
 import { globals, helpers, validators, API, } from '../../../../Config';
+import AsyncStorage from '@react-native-community/async-storage';
 
-   const LanguageModal=(props)=>{
+const LanguageModal = (props) => {
     const localize = useSelector(state => state.localize);
     const dispatch = useDispatch();
     const [modalVisible, setmodalVisible] = useState()
-    const [check,setCheck]= useState(props.language)
-    const  languagesArr= [
-        { key: 'en', label: 'english', value:helpers.getLocale(localize, "language", "English")},
-        { key: 'he', label: 'language.german', value: helpers.getLocale(localize, "language", "Lithuanian")},
+    const [check, setCheck] = useState(props.language)
+    const languagesArr = [
+        { key: 'en', label: 'english', value: helpers.getLocale(localize, "language", "English") },
+        { key: 'he', label: 'language.german', value: helpers.getLocale(localize, "language", "Lithuanian") },
     ]
 
 
 
-    const selectLanguage = (value)=>{
+    const selectLanguage = async (value) => {
         setCheck(value)
-        if (value === 'en'){
+        if (value === 'en') {
             dispatch(setTranslation("en"))
+            await AsyncStorage.setItem('AppLang', "en");
+
         }
-        else
-        {
+        else {
             dispatch(setTranslation("he"))
+            await AsyncStorage.setItem('AppLang', "he");
+
         }
-           
+
     }
-    
- 
-    const _keyExtractor = (item, index) =>  index.toString();
-       
-       return(
+
+
+    const _keyExtractor = (item, index) => index.toString();
+
+    return (
         <View style={styles.modalBackground}>
             <View style={styles.modalWrapper} >
-                <View style={ styles.modalHeading}>
-                    <TouchableOpacity style={styles.leftIcon} onPress={()=>props.close(false)}>
+                <View style={styles.modalHeading}>
+                    <TouchableOpacity style={styles.leftIcon} onPress={() => props.close(false)}>
                         <Icon name="chevron-left" size={20} color={colors.primaryColor} />
                     </TouchableOpacity>
                     <Text style={styles.headerText}>{helpers.getLocale(localize, "language", "Language")}</Text>
                 </View>
                 <View >
-                <FlatList
-                    data={languagesArr}
-                    renderItem={({ item, index }) =>
-                    
-                    <Content>
-                        <ListItem selected={false}
-                        onPress={()=>selectLanguage(item.key)} >
-                            <Left>
-                                <Text style={styles.language}>{item.value}</Text>
-                            </Left>
-                            <Right>
-                                <FastImage
-                                style={styles.radioIconStyle}
-                                source={check===item.key ? images.radioCheck : images.radioUncheck}
-                                resizeMode={"contain"}
-                                />
-                            </Right>
-                        </ListItem>
-                      
-                        {/* <View style={{borderBottomWidth:1,borderBottomColor:colors.border}}></View> */}
-                    </Content>
-                    
-                    }
-                    removeClippedSubviews={Platform.OS == "android" ? true : false}
-                    keyExtractor={_keyExtractor}
-                        />
-                        </View>
-            {/* <View style={styles.modalHeading}>
+                    <FlatList
+                        data={languagesArr}
+                        renderItem={({ item, index }) =>
+
+                            <Content>
+                                <ListItem selected={false}
+                                    onPress={() => selectLanguage(item.key)} >
+                                    <Left>
+                                        <Text style={styles.language}>{item.value}</Text>
+                                    </Left>
+                                    <Right>
+                                        <FastImage
+                                            style={styles.radioIconStyle}
+                                            source={check === item.key ? images.radioCheck : images.radioUncheck}
+                                            resizeMode={"contain"}
+                                        />
+                                    </Right>
+                                </ListItem>
+
+                                {/* <View style={{borderBottomWidth:1,borderBottomColor:colors.border}}></View> */}
+                            </Content>
+
+                        }
+                        removeClippedSubviews={Platform.OS == "android" ? true : false}
+                        keyExtractor={_keyExtractor}
+                    />
+                </View>
+                {/* <View style={styles.modalHeading}>
                 <Text style={styles.modalheadText}>Choose Language</Text>
             </View>  */}
-            {/* <View style={styles.langOptWrap}>
+                {/* <View style={styles.langOptWrap}>
                 <TouchableOpacity style={styles.langOpt}  
                 // onPress={()=>selectLanguage('en')}
                 >
@@ -129,13 +133,13 @@ import { globals, helpers, validators, API, } from '../../../../Config';
                     <Text style={{fontSize:15}}>Other Language</Text>
                 </TouchableOpacity>
             </View> */}
-        </View> 
-    </View>
+            </View>
+        </View>
 
 
-       )
+    )
 
 
 
- }
- export default LanguageModal;
+}
+export default LanguageModal;
